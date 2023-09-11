7692044a5c23ac771b8a92aeebe1b44e9568b123
Esra Garcia Gorman <esragarcia@hotmail.com>
2023-09-11 12:13:18 +0200

    Firebase veri okuma

--- app/src/main/java/com/pisiitech/firebaseveritabani/MainActivity.kt
+++ app/src/main/java/com/pisiitech/firebaseveritabani/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pisiitech.firebaseveritabani
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,7 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.pisiitech.firebaseveritabani.ui.theme.FirebaseVeritabaniTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +36,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Sayfa() {
-    ekle()
+    //ekle()
+    tumKisiler()
 }
 
 fun ekle() {
@@ -41,6 +46,29 @@ fun ekle() {
 
     val yeniKisi = Kisiler("Talat",36)
     refKisiler.push().setValue(yeniKisi)
+}
+
+
+fun tumKisiler() {
+    val db = FirebaseDatabase.getInstance()
+    val refKisiler = db.getReference("kisiler")
+
+    refKisiler.addValueEventListener(object : ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            for (d in snapshot.children) {
+                val kisi = d.getValue(Kisiler::class.java)
+
+                if(kisi != null) {
+                    Log.e("Kisi Bilgi","***************")
+                    Log.e("Kisi key",d.key!!)
+                    Log.e("Kisi ad", kisi.kisi_ad!!)
+                    Log.e("Kisi yas", kisi.kisi_yas.toString())
+                }
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) { }
+    })
 }
 
 @Preview(showBackground = true)
