646f57669a3e6a17e91874982e9324713295bf06
Esra Garcia Gorman <esragarcia@hotmail.com>
2023-09-11 12:03:15 +0200

    Firebase veri kaydi

---
+++ app/src/main/java/com/pisiitech/firebaseveritabani/Kisiler.kt
@@ -0,0 +1,7 @@
+package com.pisiitech.firebaseveritabani
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Kisiler(var kisi_ad:String? = "", var kisi_yas:Int? = 0) {
+}
